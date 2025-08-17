c1aadb0965f03dcecfd5ddae96ff701df4ea082c
ayush-mohite <76149823+ayush005-wq@users.noreply.github.com>
2025-08-18 00:26:55 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the project root
+app.use(express.static(path.join(__dirname)));
+
+// Send index.html for any other requests
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
 const OPENROUTER_KEY = process.env.OPENROUTER_KEY;
 
 // ----------- APIs -----------
